db15fa6a1d2e56d26685e0b6103fa305903effc3
oussema <oussemachaieb7@gmail.com>
2025-06-05 16:34:55 +0100

    !!

--- my_custom_app/test_application/doctype/visit_target_detail/visit_target_detail.py
+++ my_custom_app/test_application/doctype/visit_target_detail/visit_target_detail.py
@@ -3,15 +3,15 @@
 
 import frappe
 from frappe.model.document import Document
-from frappe.utils import cint, cstr
+from frappe import _
 
 
 class VisitTargetDetail(Document):
     def before_save(self):
         # Check if both customer and territory fields are empty or None
-        customer = self.get("customer")
-        territory = self.get("territory") # Use the correct field name
+        customer = self.customer
+        territory = self.territory
 
         if not customer and not territory:
             # If both are empty, raise a validation error
-            frappe.throw("Please specify either a Customer or a Territory. One of them is required.")
+            frappe.throw(_("Please specify either a Customer or a Territory. One of them is required."))
