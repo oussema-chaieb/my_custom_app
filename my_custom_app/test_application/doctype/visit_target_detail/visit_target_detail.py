0df61d86f1ab6a5519beddb2fbe8eb216a155fb5
oussema <oussemachaieb7@gmail.com>
2025-06-05 05:40:10 +0100

    finit

--- my_custom_app/test_application/doctype/visit_target_detail/visit_target_detail.py
+++ my_custom_app/test_application/doctype/visit_target_detail/visit_target_detail.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2025, DON and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import cint, cstr
 
 
 class VisitTargetDetail(Document):
-	pass
+	def before_save(self):
+		# Check if both customer and territory fields are empty or None
+        customer = self.get("customer")
+        territory = self.get("territory") # Use the correct field name
+
+        if not customer and not territory:
+            # If both are empty, raise a validation error
+            frappe.throw("Please specify either a Customer or a Territory. One of them is required.")
